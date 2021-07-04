c712696f93cf456fb4518637c981c0bdfcd59e05
vodro <vdrkowshic@gmail.com>
2021-07-04 11:48:04 +0600

    project initialized to git

---
+++ BorderPatroller/code/code/main.cpp
@@ -0,0 +1,55 @@
+/*
+ * code.cpp
+ *
+ * Created: 6/29/2021 2:29:38 PM
+ * Author : vdrko
+ */ 
+
+#include <avr/io.h>
+#ifndef F_CPU
+	#define F_CPU 1000000UL
+#endif
+
+#include <util/delay.h>
+
+#include "external_libraries/lcd/lcd.h"
+#include <string.h>
+#include <stdlib.h>
+//using namespace  std;
+class SonarUnit{
+	int distance;
+	public:
+	
+		SonarUnit(){
+			distance = 10000;
+		}
+		~SonarUnit(){
+			
+		}
+		
+		char * foo(){
+			return "I am from Sonar unit";
+		}
+	};
+
+int main(void)
+{
+    lcd_init(LCD_ON_CURSOR);
+	signed char i = 0xFF;
+	lcd_clrscr();
+	SonarUnit son;
+    while (1) 
+    {
+		
+		lcd_gotoxy(0,0);
+		char s[10];
+		itoa(i, s, 10);
+		//s[9]=0;
+		lcd_puts(s);
+		i--;
+		lcd_gotoxy(0, 1);
+		lcd_puts(son.foo());
+		_delay_ms(2000);
+    }
+}
+
---
+++ BorderPatroller/code/code/our_libraries/util.h
@@ -0,0 +1,19 @@
+/*
+ * util.h
+ *
+ * Created: 7/4/2021 10:00:55 AM
+ *  Author: vdrko
+ */ 
+
+
+#ifndef UTIL_H_
+#define UTIL_H_
+/**
+Here goes our own functions
+*/
+
+
+
+
+
+#endif /* UTIL_H_ */
